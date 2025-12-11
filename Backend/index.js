f6a511e65f57e904404eb90c00dd641e37113178
Naresh Prajapati <nareshprajapati9313@gmail.com>
2025-12-11 16:55:21 +0530

    live

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'https://vercelweb-ivea.vercel.app/',
+    origin: 'https://vercelweb-ivea.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
